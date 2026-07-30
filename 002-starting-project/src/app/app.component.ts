5bfc3c674f898a0f762196725a3af2be6bb961e3
Otniel Marques <otnielmarques3@gmail.com>
2026-07-29 23:30:54 -0300

    Directives module finished

--- 002-starting-project/src/app/app.component.ts
+++ 002-starting-project/src/app/app.component.ts
@@ -5,13 +5,15 @@ import { LearningResourcesComponent } from './learning-resources/learning-resour
 import { AuthService } from './auth/auth.service';
 //import { NgIf } from "../../node_modules/@angular/common/index";
 import { SafeLinkDirective } from './safe-link.directive';
+import { AuthDirective } from "./auth/auth.directive";
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, /*NgIf*/],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective, LogDirective],
 })
 export class AppComponent {
   private authService = inject(AuthService);
--- 002-starting-project/src/app/safe-link.directive.ts
+++ 002-starting-project/src/app/safe-link.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
+import { LogDirective } from "./log.directive";
 
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
     host: {
         '(click)': 'onConfirmLeavePage($event)'
-    }
+    },
+    hostDirectives: [LogDirective]
 })
 export class SafeLinkDirective {
     queryParam = input('myapp', {alias: 'appSafeLink'});
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
     constructor(){
         console.log('SafeLinkDirective is active!')
     }
@@ -17,9 +20,9 @@ export class SafeLinkDirective {
         const wantsToLeave = window.confirm('Do you want to leave the app?');
 
         if(wantsToLeave){
-            const address = (event.target as  HTMLAnchorElement).href;
+            const address = this.hostElementRef.nativeElement.href;
 
-            (event.target as  HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
             return;
         }
 
